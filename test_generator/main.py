650634c0ecce504d02adcf54ed6cd6d586b4f81e
Brian Fu <fubof@sfu.ca>
2020-05-01 23:21:12 -0700

    Basic error test case generator error handling, best case generated

--- test_generator/main.py
+++ test_generator/main.py
@@ -4,19 +4,42 @@
 #Sizes 10-10000
 #We are using Next-Fit Decreasing Height (NFDH)
 
-#We might need more width units (if no floats) to diff between 10000 rectangles
 #normal var is the texture atlas width (inf. height, finite set width); All values given will be relative to this var
+
+#All written test cases will have a newline after the last entry
 
 def main():
 
     name = input("Test case filename: ") #This will overwrite existing test cases
 
+    # Make sure only 1 of the funcs is ran at once, manually comment out the rest
+    testcases = 6
+    print("The possible generated test cases:")
+    print("1) Diagonal Case")
+    print("2) Best Case")
+    print("3) Worst Case")
+    print("4) All Pairs Case")
+    print("5) Filled (No Gaps) Case")
+    print("6) Filled (With Gaps) Case")
+
+    test = int(input("Please choose a test case number: "))
+    assert test > 0 and test <= testcases, "Please enter an existing test case" #Throws AssertionError if bad choice
+
     with open(name+'.txt', 'w', encoding='utf-8') as outfile:
-        #Make sure only 1 of the funcs is ran at once, manually comment out the rest
+
         res = [] #All functions should return a list of ints
-        res = diagonal_case()
-
-
+        if (test == 1): #Switch case on res
+            res = diagonal_case()
+        elif(test == 2):
+            res = best_case()
+        elif(test == 3):
+            res = worst_case()
+        elif(test == 4):
+            res = allpairs_case()
+        elif(test == 5):
+            res = filled_nogaps_case()
+        elif(test == 6):
+            res = filled_withgaps_case()
 
         randomizer(res) #Comment this out when need not random output
         res_str = linewriter(res)
@@ -43,13 +66,18 @@ def diagonal_case(): #Largest item first, each item is smaller than the last (si
     n = 10000
     res.append(f'{normal} {n}')
 
-    for i in reversed(range(1,10001)):
+    for i in reversed(range(1,n+1)):
         res.append(f'{i} {i}')
     return res
 
 def best_case(): #No 2 items can go in the same bin, largest size (10000)
-    normal = 10000
-    pass
+    res = []
+    normal = 10
+    n = 10
+    res.append(f'{normal} {n}')
+    for h in reversed(range(1,n+1)):
+        res.append(f'{h} {1}') #all items have width 1
+    return res
 
 def worst_case(): #All items can fit in the same bin (width 4-6), smallest size (10)
     normal = 10
